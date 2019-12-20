cc19aed6a732a434ed1b2b14d88cf6a9ff925abf
rarestoma <tomairares@gmail.com>
2019-12-20 15:05:33 +0200

    updated version to v1.1.1

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -777,7 +777,7 @@ var config = {
 
     	$.notify({
         	icon: "tim-icons icon-bell-55",
-        	message: "Welcome to <b>Black Dashboard</b> - a beautiful freebie for every web developer."
+        	message: "Welcome to <b>Argon Dashboard</b> - a beautiful freebie for every web developer."
 
         },{
             type: type[color],
